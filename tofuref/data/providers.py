dbae0e82059e1deae957f476476a435bfbb847bb
David Jetelina <david@djetelina.cz>
2025-05-01 18:00:45 +0200

    refactor(data): Fetch data from API

--- tofuref/data/providers.py
+++ tofuref/data/providers.py
@@ -1,159 +1,118 @@
-import json
 from dataclasses import dataclass, field
-from pathlib import Path
-from typing import Dict, Optional
-from asyncio import subprocess
+from typing import Dict, Optional, List
+from rich.json import JSON as rich_json
+import json
 
-from tofuref.data.helpers import get_repo_dir, header_markdown_split
-from tofuref.data.resources import Resource
+from tofuref.data.resources import Resource, ResourceType
+from tofuref.data.helpers import (
+    get_registry_api,
+    header_markdown_split,
+)
 
 
 @dataclass
 class Provider:
     organization: str
     name: str
-    repository_url: str
-    version: str
-    index: str = "This provider doesn't have an index"
-    ready: bool = False
-    _version: str = field(init=False)
-    docs_dir: Optional[Path] = None
-    resource_dir: Optional[Path] = None
-    resources: Dict[str, Resource] = field(default_factory=dict)
-    data_sources_dir: Optional[Path] = None
-    data_sources: Dict[str, Resource] = field(default_factory=dict)
+    description: str
+    fork_count: int
+    blocked: bool
+    popularity: int
+    _overview: Optional[str] = None
+    _active_version: Optional[str] = None
+    versions: List[Dict[str, str]] = field(default_factory=list)
+    fork_of: Optional[str] = None
+    raw_json: Optional[dict] = None
+    resources: List[Resource] = field(default_factory=list)
+    datasources: List[Resource] = field(default_factory=list)
+    functions: List[Resource] = field(default_factory=list)
+    guides: List[Resource] = field(default_factory=list)
+
+    @classmethod
+    def from_json(cls, data: dict) -> "Provider":
+        return cls(
+            organization=data["addr"]["namespace"],
+            name=data["addr"]["name"],
+            description=data["description"],
+            fork_count=data["fork_count"],
+            blocked=data["is_blocked"],
+            popularity=data["popularity"],
+            versions=data["versions"],
+            fork_of=data.get("fork_of", {}).get("display"),
+            raw_json=data,
+        )
 
     @property
-    def repo_dir(self) -> Path:
-        return get_repo_dir(f"{self.organization}_{self.name}_{self.version}")
+    def display_name(self) -> str:
+        return f"{self.organization}/{self.name}"
 
     @property
-    def version(self):
-        return self._version
+    def active_version(self):
+        if self._active_version is None:
+            self._active_version = self.versions[0]["id"]
+        return self._active_version
 
-    @version.setter
-    def version(self, value):
-        self._version = value
-        self.ready = False
+    @property
+    def _endpoint(self) -> str:
+        return f"{self.organization}/{self.name}/{self.active_version}"
+
+    async def overview(self):
+        if self._overview is None:
+            self._overview = await get_registry_api(
+                f"{self._endpoint}/index.md", json=False
+            )
+            _, self._overview = header_markdown_split(self._overview)
+        return self._overview
 
     async def load_resources(self):
-        if not self.ready:
-            await self.ensure_repo()
-            self._find_docs()
-            self._extract_docs()
-            self.ready = True
-
-    async def ensure_repo(self):
-
-        if self.repo_dir.exists() and (self.repo_dir / ".git").exists():
-            # We checked out a specific version, that is immutable
-            # We could have checked self.ready technically, but due to version let's leave it like this :)
-            return
-        else:
-            self.repo_dir.parent.mkdir(parents=True, exist_ok=True)
-            # TODO --single-branch --branch TAG
-            clone = await subprocess.create_subprocess_exec(
-                "git",
-                "clone",
-                "--depth",
-                "1",
-                self.repository_url,
-                str(self.repo_dir),
-                stdin=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            await clone.wait()
-
-    def _find_docs(self):
-        # Need to check website first, because docs can be used for repo docs (e.g. opentofu/aws)
-        self.docs_dir = (
-            self.repo_dir / "website" / "docs"
-            if (self.repo_dir / "website" / "docs").exists()
-            else self.repo_dir / "docs"
+        resource_data = await get_registry_api(
+            f"{self.organization}/{self.name}/{self.active_version}/index.json"
         )
-        possible_resources = {
-            "long": self.docs_dir / "resources",
-            "short": self.docs_dir / "r",
-            "cdk_py_long": self.docs_dir / "cdktf" / "python" / "resources",
-            "cdk_ts_long": self.docs_dir / "cdktf" / "typescript" / "resources",
-            "cdk_py_short": self.docs_dir / "cdktf" / "python" / "r",
-            "cdk_ts_short": self.docs_dir / "cdktf" / "typescript" / "r",
-        }
-        docs = [k for k, v in possible_resources.items() if v.exists()]
-        if not docs:
-            # Giving up for now. In theory provider can have 0 resources and some data sources, or even just functions, but let's not worry about it for now
-            return
-        docs_format = docs[0]
-        data_sources_subdir_name = "data-sources" if "long" in docs_format else "d"
-        self.resources_dir = possible_resources[docs_format]
-        self.data_sources_dir = self.resources_dir.parent / data_sources_subdir_name
-        if not self.data_sources_dir.exists():
-            self.data_sources_dir = None
+        for g in sorted(resource_data["docs"]["guides"], key=lambda x: x["name"]):
+            self.resources.append(Resource(g["name"], self, type=ResourceType.GUIDE))
 
-    def _extract_docs(self):
-        for f in self.docs_dir.iterdir():
-            if "index" in f.name:
-                _, self.index = header_markdown_split(f.read_text())
-        resource_types = {
-            "resource": {
-                "dir": self.resources_dir,
-                "target": self.resources,
-            },
-            "data_source": {
-                "dir": self.data_sources_dir,
-                "target": self.data_sources,
-            },
-        }
-        for resource_type, info in resource_types.items():
-            if info["dir"] is None:
-                continue
-            for file in info["dir"].iterdir():
-                if file.suffix not in [".md", ".markdown"]:
-                    continue
-                contents = file.read_text()
-                header, markdown_content = header_markdown_split(contents)
-                real_stem = file.stem.replace(".html", "")
-                info["target"][real_stem] = Resource(
-                    provider=self,
-                    name=real_stem,
-                    description=header.get("description", ""),
-                    type=resource_type,
-                    content=markdown_content,
-                )
+        for r in sorted(resource_data["docs"]["resources"], key=lambda x: x["name"]):
+            self.resources.append(Resource(r["name"], self, type=ResourceType.RESOURCE))
+        for d in sorted(resource_data["docs"]["datasources"], key=lambda x: x["name"]):
+            self.resources.append(
+                Resource(d["name"], self, type=ResourceType.DATASOURCE)
+            )
+        for f in sorted(resource_data["docs"]["functions"], key=lambda x: x["name"]):
+            self.resources.append(Resource(f["name"], self, type=ResourceType.FUNCTION))
+
+    def __rich__(self):
+        return rich_json(
+            json.dumps(
+                {
+                    k: v
+                    for k, v in self.__dict__.items()
+                    if k not in ["raw_json", "versions"]
+                }
+            )
+        )
 
 
-def populate_providers(registry_dir) -> Dict[str, Provider]:
+async def populate_providers() -> Dict[str, Provider]:
     providers = {}
 
-    providers_dir = registry_dir / "providers"
-    if not providers_dir.exists():
-        return providers
+    data = await get_registry_api("index.json")
 
-    for first_level_dir in providers_dir.iterdir():
-        for organization in first_level_dir.iterdir():
-            for provider_file in organization.iterdir():
-                if provider_file.is_dir():
-                    # Fuck them, okay?
-                    continue
-                name = provider_file.name.replace(".json", "")
-                full_name = f"{organization.stem}/{name}"
+    for provider_json in data["providers"]:
+        provider = Provider.from_json(provider_json)
+        filter_in = (
+            provider.versions,
+            not provider.blocked,
+            (not provider.fork_of or provider.organization == "opentofu"),
+            provider.organization not in ["terraform-providers"],
+        )
+        if all(filter_in):
+            providers[provider.display_name] = provider
 
-                with open(provider_file, "r") as f:
-                    versions_data = json.load(f)
-
-                repo_url = versions_data["versions"][0]["shasums_url"].split(
-                    "releases"
-                )[0]
-
-                providers[full_name] = Provider(
-                    organization=organization.stem,
-                    name=name,
-                    resources={},
-                    data_sources={},
-                    # Let's assume they are ordered for now
-                    version=versions_data["versions"][0]["version"],
-                    ready=False,
-                    repository_url=repo_url,
-                )
+    providers = {
+        k: v
+        for k, v in sorted(
+            providers.items(), key=lambda p: p[1].popularity, reverse=True
+        )
+    }
 
     return providers
